37998fe1202bb992b4b8d907add493e62c4085a2
David Santiago Calderon Idarraga <santizcaida@gmail.com>
2026-05-25 11:40:20 -0500

    Add density calculation and timestamp formatting

--- Proyecto_Primera_Entrega_Distribuidos/PC1/sensores/SensorGPS.java
+++ Proyecto_Primera_Entrega_Distribuidos/PC1/sensores/SensorGPS.java
@@ -1,6 +1,8 @@
 package sensores;
 
-import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class SensorGPS extends Sensor {
 
@@ -10,14 +12,15 @@ public class SensorGPS extends Sensor {
 
     @Override
     public String generarEvento() {
-    	int velocidad = (rand.nextInt(100) < 85) ? 11 + rand.nextInt(50) : rand.nextInt(10);
-        String nivel = (velocidad < 10) ? "ALTA" : ((velocidad <= 39) ? "NORMAL" : "BAJA");
-        //int velocidad = rand.nextInt(50);
-        //String nivel = (velocidad < 10) ? "ALTA" : ((velocidad <= 39) ? "NORMAL" : "BAJA");
+        int velocidad = (rand.nextInt(100) < 85) ? 11 + rand.nextInt(50) : rand.nextInt(10);
+        int densidad = (velocidad < 20) ? 25 + rand.nextInt(15) : rand.nextInt(15); 
+        
+        String timestampLocal = ZonedDateTime.now(ZoneId.of("America/Bogota"))
+                                             .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
 
         return String.format(
-            "{\"sensor_id\": \"%s\", \"tipo_sensor\": \"%s\", \"interseccion\": \"%s\", \"direccion\": \"%s\", \"nivel_congestion\": \"%s\", \"velocidad_promedio\": %d, \"timestamp\": \"%s\"}",
-            sensorId, tipo, interseccion, direccion, nivel, velocidad, Instant.now().toString()
+            "{\"sensor_id\": \"%s\", \"tipo_sensor\": \"%s\", \"interseccion\": \"%s\", \"direccion\": \"%s\", \"densidad\": %d, \"velocidad_promedio\": %d, \"timestamp\": \"%s\"}",
+            sensorId, tipo, interseccion, direccion, densidad, velocidad, timestampLocal
         );
     }
 }
