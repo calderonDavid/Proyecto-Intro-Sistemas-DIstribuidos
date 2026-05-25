69a4c484605305b228aaaeeba8b076d8607553d8
David Santiago Calderon Idarraga <santizcaida@gmail.com>
2026-05-25 11:40:00 -0500

    Refactor timestamp generation using ZonedDateTime

--- Proyecto_Primera_Entrega_Distribuidos/PC1/sensores/SensorEspira.java
+++ Proyecto_Primera_Entrega_Distribuidos/PC1/sensores/SensorEspira.java
@@ -1,6 +1,8 @@
 package sensores;
 
-import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class SensorEspira extends Sensor {
 
@@ -12,9 +14,12 @@ public class SensorEspira extends Sensor {
     public String generarEvento() {
         int vehiculos = (rand.nextInt(100) < 85) ? rand.nextInt(10) : 11 + rand.nextInt(10);
         
-        //int vehiculos = rand.nextInt(15);
-        String timestampInicio = Instant.now().minusSeconds(30).toString();
-        String timestampFin = Instant.now().toString();
+        ZonedDateTime tiempoFin = ZonedDateTime.now(ZoneId.of("America/Bogota"));
+        
+        ZonedDateTime tiempoInicio = tiempoFin.minusSeconds(30);
+
+        String timestampInicio = tiempoInicio.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        String timestampFin = tiempoFin.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
 
         return String.format(
             "{\"sensor_id\": \"%s\", \"tipo_sensor\": \"%s\", \"interseccion\": \"%s\", \"direccion\": \"%s\", \"vehiculos_contados\": %d, \"intervalo_segundos\": 30, \"timestamp_inicio\": \"%s\", \"timestamp_fin\": \"%s\"}",
