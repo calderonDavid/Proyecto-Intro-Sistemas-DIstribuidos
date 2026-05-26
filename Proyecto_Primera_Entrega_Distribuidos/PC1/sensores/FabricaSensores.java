c976fdbcbe525ef4a44f9a852756fd3958b52e69
David Santiago Calderon Idarraga <santizcaida@gmail.com>
2026-05-25 19:47:17 -0500

    Update FabricaSensores.java

--- Proyecto_Primera_Entrega_Distribuidos/PC1/sensores/FabricaSensores.java
+++ Proyecto_Primera_Entrega_Distribuidos/PC1/sensores/FabricaSensores.java
@@ -5,20 +5,22 @@ import java.util.List;
 
 public class FabricaSensores {
 
-    public static List<Sensor> crearSensoresPorInterseccion(String interseccion) {
+    public static List<Sensor> crearSensoresPorInterseccion(String interseccion, int cantidadPorTipo) {
         List<Sensor> sensores = new ArrayList<>();
 
-        // Derecha (R) - Se agrega el parámetro interseccion al constructor
-        sensores.add(new SensorCamara("CAM-" + interseccion + "-R", "R", interseccion));
-        sensores.add(new SensorEspira("ESP-" + interseccion + "-R", "R", interseccion));
-        sensores.add(new SensorGPS("GPS-" + interseccion + "-R", "R", interseccion));
+        // Iteramos según la cantidad solicitada (Escenario 1 = 1 vez, Escenario 2 = 2 veces)
+        for (int i = 1; i <= cantidadPorTipo; i++) {
+            // Derecha (R) - Añadimos el número 'i' al ID para que no se repitan los nombres
+            sensores.add(new SensorCamara("CAM-" + interseccion + "-R-" + i, "R", interseccion));
+            sensores.add(new SensorEspira("ESP-" + interseccion + "-R-" + i, "R", interseccion));
+            sensores.add(new SensorGPS("GPS-" + interseccion + "-R-" + i, "R", interseccion));
 
-        // Abajo (D) - Se agrega el parámetro interseccion al constructor
-        sensores.add(new SensorCamara("CAM-" + interseccion + "-D", "D", interseccion));
-        sensores.add(new SensorEspira("ESP-" + interseccion + "-D", "D", interseccion));
-        sensores.add(new SensorGPS("GPS-" + interseccion + "-D", "D", interseccion));
+            // Abajo (D)
+            sensores.add(new SensorCamara("CAM-" + interseccion + "-D-" + i, "D", interseccion));
+            sensores.add(new SensorEspira("ESP-" + interseccion + "-D-" + i, "D", interseccion));
+            sensores.add(new SensorGPS("GPS-" + interseccion + "-D-" + i, "D", interseccion));
+        }
 
         return sensores;
     }
 }
-
