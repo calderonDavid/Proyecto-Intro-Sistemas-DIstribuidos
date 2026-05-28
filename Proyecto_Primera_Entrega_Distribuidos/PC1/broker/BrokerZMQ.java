501fa53aed888ffc8c936198d41adb271476d509
David Santiago Calderon Idarraga <santizcaida@gmail.com>
2026-05-27 22:56:52 -0500

    Update BrokerZMQ.java

--- Proyecto_Primera_Entrega_Distribuidos/PC1/broker/BrokerZMQ.java
+++ Proyecto_Primera_Entrega_Distribuidos/PC1/broker/BrokerZMQ.java
@@ -4,33 +4,34 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
+// Clase que recibe los datos de los sensores locales y los reenvía
 public class BrokerZMQ {
 
     public static void main(String[] args) {
+        //contexto de ZeroMQ para manejar las conexiones
         try (ZContext context = new ZContext()) {
             
-            // Escucha a los sensores generados en PublisherSensores Puerto 5554
-            
+            //socket de tipo suscriptor para escuchar a los sensores locales
             ZMQ.Socket localPub = context.createSocket(SocketType.SUB);
             localPub.bind("tcp://127.0.0.1:5554");
+            //nos suscribimos a los mensajes que tengan el tema "TRAFICO"
             localPub.subscribe("TRAFICO".getBytes());
 
-            // PUB Expone los datos para que el PC2 se conecte (Puerto 5555)
+            //creamos un socket de tipo publish para enviar los datos hacia el PC2
             ZMQ.Socket enrutador = context.createSocket(SocketType.PUB);
-            
             enrutador.bind("tcp://*:5555");
 
             System.out.println("BROKER ZMQ Iniciado y enrutando tráfico.");
 
-            //Bucle infinito mandando mensajes de PC1 a PC2
+            //bucle infinito para mantener el broker activo recibiendo y enviando mensajes
             while (!Thread.currentThread().isInterrupted()) {
             
-                // Recibe el tópico y el mensaje cifrado de generadorSensores
+                //recibimos la primera parte del mensaje que corresponde al (tópico)
                 byte[] topic = localPub.recv();
-                
+                //recibimos la segunda parte que contiene el evento cifrado del sensor
                 byte[] message = localPub.recv();
 
-                // Lo reenvía inmediatamente hacia el PC2
+                // Reenviamos el mensaje hacia el PC2 manteniendo la estructura (tema + mensaje)
                 enrutador.sendMore(topic);       
                 enrutador.send(message);
             }
