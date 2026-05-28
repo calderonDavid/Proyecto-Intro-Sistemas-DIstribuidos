8f4b670985ac731653fa22fc3496357bdeadc717
David Santiago Calderon Idarraga <santizcaida@gmail.com>
2026-05-27 22:53:52 -0500

    Refactor PublisherSensores for improved clarity

--- Proyecto_Primera_Entrega_Distribuidos/PC1/sensores/PublisherSensores.java
+++ Proyecto_Primera_Entrega_Distribuidos/PC1/sensores/PublisherSensores.java
@@ -12,27 +12,29 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+//clase principal para la generación y publicación de los eventos de los sensores
 public class PublisherSensores {
 
+    // Una cola para almacenar los eventos generados antes de enviarlos. Soporta hasta 2000 elementos
     private static final BlockingQueue<String[]> colaEventos = new LinkedBlockingQueue<>(2000);
 
     public static void main(String[] args) {
         
-        // Variables por defecto
+        // Configuraciones iniciales por defecto
         boolean esMultihilo = true;
         int escenario = 1;
 
-        // Lectura de parámetros al ejecutar en consola
+        //evaluar si se manda para multihilo
         if (args.length >= 2) {
             esMultihilo = args[0].equalsIgnoreCase("multihilo");
             escenario = Integer.parseInt(args[1]);
         }
 
-        // Configuración exigida por la Tabla 1 del proyecto
+        // Definimos la cantidad de sensores y los tiempos de envío
         int cantidadSensoresPorTipo = (escenario == 2) ? 2 : 1;
         int intervaloSegundos = (escenario == 2) ? 5 : 10;
         
-        // LA MAGIA DEL RENDIMIENTO: 10 hilos concurrentes vs 1 solo hilo cuello de botella
+        // Si usamos multihilo, habilitamos 10 hilos o todo corre en 1 hilo
         int cantidadHilos = esMultihilo ? 10 : 1; 
 
         System.out.println("=== PRUEBA DE RENDIMIENTO INICIADA ===");
@@ -40,6 +42,7 @@ public class PublisherSensores {
         System.out.println("Carga: Escenario " + escenario + " (" + cantidadSensoresPorTipo + " sensor(es) por tipo cada " + intervaloSegundos + " seg)");
         System.out.println("======================================\n");
 
+        // Creamos todos los sensores usando las intersecciones generadas
         List<String> intersecciones = GeneradorIntersecciones.getTodas();
         List<Sensor> todosLosSensores = new ArrayList<>();
 
@@ -47,16 +50,21 @@ public class PublisherSensores {
             todosLosSensores.addAll(FabricaSensores.crearSensoresPorInterseccion(inter, cantidadSensoresPorTipo));
         }
 
+        //iniciamos un hilo en segundo plano dedicado exclusivamente a enviar los datos acumulados en la cola
         Thread zmqPublisherThread = new Thread(PublisherSensores::publicarEventosZMQ);
         zmqPublisherThread.start();
 
+        //configuramos los hilos para que los sensores empiecen a trabajar
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(cantidadHilos);
 
         for (Sensor sensor : todosLosSensores) {
+            //cada sensor va a generar un evento cada 'x' segundos según el escenario
             scheduler.scheduleAtFixedRate(() -> {
                 try {
                     String jsonEvento = sensor.generarEvento();
                     String eventoCifrado = CryptoUtils.encrypt(jsonEvento);
+                    
+                    // Metemos el evento en la cola para que el publicador lo envíe luego
                     colaEventos.put(new String[]{"TRAFICO", eventoCifrado});
                     
                     System.out.println("[PC1] Evento generado -> " + sensor.getSensorId());
@@ -67,12 +75,15 @@ public class PublisherSensores {
         }
     }
 
+    // función que para sacar los eventos de la cola y los manda mediante ZeroMQ
     private static void publicarEventosZMQ() {
         try (ZContext context = new ZContext()) {
             ZMQ.Socket publisher = context.createSocket(SocketType.PUB);
             publisher.bind("tcp://*:5555");
 
+            // Bucle infinito que funciona mientras el programa esté encendido
             while (!Thread.currentThread().isInterrupted()) {
+                // Toma un evento de la cola (si la cola está vacía, espera automáticamente)
                 String[] datos = colaEventos.take(); 
                 publisher.sendMore(datos[0]); 
                 publisher.send(datos[1]);     
