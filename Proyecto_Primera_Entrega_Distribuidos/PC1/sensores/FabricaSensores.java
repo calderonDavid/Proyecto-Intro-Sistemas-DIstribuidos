20cec228be69d1888afef9e21f49b043975998ee
David Santiago Calderon Idarraga <santizcaida@gmail.com>
2026-05-27 22:54:26 -0500

    Update FabricaSensores.java

--- Proyecto_Primera_Entrega_Distribuidos/PC1/sensores/FabricaSensores.java
+++ Proyecto_Primera_Entrega_Distribuidos/PC1/sensores/FabricaSensores.java
@@ -3,19 +3,22 @@ package sensores;
 import java.util.ArrayList;
 import java.util.List;
 
+// patron factory
 public class FabricaSensores {
 
+    // Función que devuelve una lista de sensores listos para una intersección específica
     public static List<Sensor> crearSensoresPorInterseccion(String interseccion, int cantidadPorTipo) {
         List<Sensor> sensores = new ArrayList<>();
 
-        // Iteramos según la cantidad solicitada (Escenario 1 = 1 vez, Escenario 2 = 2 veces)
+        // Creamos la cantidad de sensores solicitada
         for (int i = 1; i <= cantidadPorTipo; i++) {
-            // Derecha (R) - Añadimos el número 'i' al ID para que no se repitan los nombres
+            
+            // Se usa el número 'i' en el ID para diferenciarlos si hay más de uno (right)
             sensores.add(new SensorCamara("CAM-" + interseccion + "-R-" + i, "R", interseccion));
             sensores.add(new SensorEspira("ESP-" + interseccion + "-R-" + i, "R", interseccion));
             sensores.add(new SensorGPS("GPS-" + interseccion + "-R-" + i, "R", interseccion));
 
-            // Abajo (D)
+            // Sensores para la vía que va hacia abajo (D de Down)
             sensores.add(new SensorCamara("CAM-" + interseccion + "-D-" + i, "D", interseccion));
             sensores.add(new SensorEspira("ESP-" + interseccion + "-D-" + i, "D", interseccion));
             sensores.add(new SensorGPS("GPS-" + interseccion + "-D-" + i, "D", interseccion));
