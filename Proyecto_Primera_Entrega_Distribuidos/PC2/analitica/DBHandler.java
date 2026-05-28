d630109686a2ecf0196f4efbe0a9ec3603a24c19
David Santiago Calderon Idarraga <santizcaida@gmail.com>
2026-05-27 22:59:43 -0500

    Update DBHandler.java

--- Proyecto_Primera_Entrega_Distribuidos/PC2/analitica/DBHandler.java
+++ Proyecto_Primera_Entrega_Distribuidos/PC2/analitica/DBHandler.java
@@ -10,6 +10,7 @@ import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
 public class DBHandler implements Runnable {
+    // Parámetros de conexión a la base local
     private static final String MONGO_URI = "mongodb://localhost:27017";
     private static final String DATABASE_NAME = "bd_trafico_replica";
 
@@ -22,17 +23,21 @@ public class DBHandler implements Runnable {
             MongoCollection<Document> colSensores = database.getCollection("historico_sensores");
             MongoCollection<Document> colSemaforos = database.getCollection("historico_semaforos");
 
+            // Socket PULL dedicado para recibir lo que le envían internamente desde la clase Subscriber
             ZMQ.Socket pullSocket = context.createSocket(SocketType.PULL);
-
             pullSocket.bind("tcp://*:5556"); 
 
             System.out.println("[BD REPLICA] Persistencia iniciada en MongoDB local...");
 
+            // Bucle infinito escuchando datos para insertar en la BD
             while (!Thread.currentThread().isInterrupted()) {
                 String jsonRecibido = pullSocket.recvStr();
                 if (jsonRecibido != null) {
                     try {
+                        // Parseamos el JSON a formato BSON para poder guardarlo en Mongo
                         Document doc = Document.parse(jsonRecibido);
+                        
+                        // Determinamos a qué colección va la data dependiendo de sus campos
                         if (doc.containsKey("tipo_sensor")) {
                             colSensores.insertOne(doc);
                         } else if (doc.containsKey("estado_nuevo")) {
