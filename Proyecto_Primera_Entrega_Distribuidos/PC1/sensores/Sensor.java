d60731cc11818c0be74a9ce3233849116148c38f
David Santiago Calderon Idarraga <santizcaida@gmail.com>
2026-05-27 22:55:04 -0500

    Enhance Sensor class with comments and constructor
    Added comments for clarity and updated constructor.

--- Proyecto_Primera_Entrega_Distribuidos/PC1/sensores/Sensor.java
+++ Proyecto_Primera_Entrega_Distribuidos/PC1/sensores/Sensor.java
@@ -2,14 +2,17 @@ package sensores;
 
 import java.util.Random;
 
+// Clase bpara la plantilla de sensores
 public abstract class Sensor {
 
-    protected String sensorId;
-    protected String tipo;
-    protected String direccion;     // "R" o "D"
-    protected String interseccion;  // Ej: "C5"
-    protected Random rand;
 
+    protected String sensorId;      // Identificador único del dispositivo
+    protected String tipo;          // Define si es cámara, espira o GPS
+    protected String direccion;     // Sentido del flujo vehicular (R o D)
+    protected String interseccion;  // Lugar donde está ubicado el sensor 
+    protected Random rand;          // Generador de números aleatorios para simular variaciones en el tráfico
+
+    // Constructor
     public Sensor(String sensorId, String tipo, String direccion, String interseccion) {
         this.sensorId = sensorId;
         this.tipo = tipo;
@@ -18,13 +21,12 @@ public abstract class Sensor {
         this.rand = new Random();
     }
 
+    // Método que cada tipo de sensor
     public abstract String generarEvento();
 
+    // Métodos para consultar la información del sensor
     public String getSensorId() { return sensorId; }
-    
     public String getTipo() { return tipo; }
-    
     public String getDireccion() { return direccion; }
-    
     public String getInterseccion() { return interseccion; }
 }
